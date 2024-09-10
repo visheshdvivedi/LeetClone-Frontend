488a0dd797470a0591ae89f2e18223ffa9e64ece
visheshdvivedi <visheshdvivedi5012@gmail.com>
2024-09-10 17:34:58 +0530

    update navbar not opening on mobile bug

--- src/parts/Header.jsx
+++ src/parts/Header.jsx
@@ -27,6 +27,7 @@ const Header = () => {
     ]
     const navigate = useNavigate();
     const { isAuthenticated, user } = React.useContext(AuthContext);
+    const [openNavbar, setOpenNavbar] = React.useState(true);
     const [profilePic, setProfilePic] = React.useState('/profile_pic.png');
 
     const updateProfilePicture = async () => {
@@ -56,13 +57,13 @@ const Header = () => {
                     <img src={icon} width={15} className='me-2' alt="profile-picture" />
                     <a className="flex-none text-xl font-semibold text-black dark:text-white" href="#">LeetClone</a>
                     <div className="sm:hidden">
-                        <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10" data-hs-collapse="#navbar-with-mega-menu" aria-controls="navbar-with-mega-menu" aria-label="Toggle navigation">
+                        <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10" data-hs-collapse="#navbar-with-mega-menu" aria-controls="navbar-with-mega-menu" aria-label="Toggle navigation" onClick={() => setOpenNavbar(!openNavbar)}>
                             <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
                             <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
                         </button>
                     </div>
                 </div>
-                <div id="navbar-with-mega-menu" className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+                <div id="navbar-with-mega-menu" className={`hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block ${openNavbar ? "hidden" : ""}`}>
                     <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
 
                         {/* <button onClick={toggleDarkMode} className='px-2 py-1 dark:text-white'>
